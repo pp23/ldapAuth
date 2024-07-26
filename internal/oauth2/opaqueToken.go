801ddc5e5ae86d5fbea3e62f76ca9873c6848f0b
Paul Peschel <paul.peschel@posteo.de>
2024-07-26 07:37:01 +0200

    Send opaque token on request
    Exchanges a provided auth code by a token. This is an opaque
    token which means it has no specific data like user related data which
    should kept secret from the client. Instead the opaque token gets
    replaced by a structured token when a secured API endpoint gets called.
    This way, the user agent does not get detailed data of the JWT.

--- internal/oauth2/opaqueToken.go
+++ internal/oauth2/opaqueToken.go
@@ -1,9 +1,25 @@
 package oauth2
 
 import (
+	"encoding/json"
 	"fmt"
+	"log"
 	"net/http"
+	"net/url"
 )
+
+// rfc6749 4.1.3 Access Token Request
+// grant_type   - REQUIRED. must be "authorization_code"
+// code         - REQUIRED. authcode received from authorization server.
+// redirect_uri - REQUIRED, if redirect_uri was included in auth request, value must be identical.
+// client_id    - REQUIRED, if client is not authenticating with authorization server.
+
+type OpaqueTokenRequest struct {
+	GrantType   string
+	Code        string
+	RedirectURI *url.URL
+	ClientID    string
+}
 
 // rfc6749 4.1.4 Access Token Response
 // rfc6749 5.1.
@@ -14,33 +30,62 @@ import (
 // scope         - OPTIONAL, if identical to requested scope, otherwise REQUIRED.
 
 type OpaqueToken struct {
-	access_token  string
-	token_type    string
-	expires_in    uint64
-	refresh_token string
-	scope         string
+	AccessToken  string `json:"access_token"`
+	TokenType    string `json:"token_type"`
+	ExpiresIn    uint64 `json:"expires_in"`
+	RefreshToken string `json:"refresh_token,omitempty"`
+	Scope        string `json:"scope,omitempty"`
 }
 
 func IsOpaqueTokenRequest(req *http.Request) bool {
 	// TODO: Consider more checks based on other parameters
+	err := req.ParseForm()
+	if err != nil {
+		log.Print(err)
+		return false
+	}
 	return req.FormValue("grant_type") == "authorization_code"
 }
 
 // Create an OpaqueToken from HTTP request
 func OpaqueTokenFromRequest(req *http.Request) (*OpaqueToken, error) {
-	if req.FormValue("grant_type") != "authorization_code" {
+	err := req.ParseForm()
+	if err != nil {
+		log.Print(err)
 		return nil, fmt.Errorf("invalid_request")
 	}
+	opaqueTokenRequest := OpaqueTokenRequest{
+		GrantType:   req.FormValue("grant_type"),
+		Code:        req.FormValue("code"),
+		RedirectURI: nil,
+		ClientID:    req.FormValue("client_id"),
+	}
+	if opaqueTokenRequest.GrantType != "authorization_code" {
+		return nil, fmt.Errorf("invalid_request")
+	}
+
+	// authentcating a webapp client which runs of the users machine:
+	// intermediate client secret is encrypted in auth code
+	// check client_id and decrypted client secret from auth code with cached values
+
 	// TODO: invalid_client, client auth failed
 	// TODO: invalid_grant, provided authorization grant invalid
 	// TODO: unauthorized_client, client not authorized to use this authorization grant type
 	// TODO: unsupported_grant_type, auth grant type not supported by server
 	// TODO: invalid_scope, requested scope is invalid
 	return &OpaqueToken{
-		access_token:  "TODO",
-		token_type:    "bearer",
-		expires_in:    600,
-		refresh_token: "TODO",
-		scope:         "TODO",
+		AccessToken:  "TODO-UUID-OR-WHATEVER-0000000000000000000000000000000000000000",
+		TokenType:    "bearer",
+		ExpiresIn:    600,
+		RefreshToken: "TODO",
+		Scope:        "TODO",
 	}, nil
+}
+
+func (token *OpaqueToken) AccessTokenJson() ([]byte, error) {
+	b, err := json.Marshal(token)
+	if err != nil {
+		return []byte{}, err
+	}
+	return b, nil
 }
--- ldapauth_test.go
+++ ldapauth_test.go
@@ -4,6 +4,7 @@ package ldapAuth_test
 
 import (
 	"context"
+	"encoding/json"
 	"io"
 	"net"
 	"net/http"
@@ -167,10 +168,11 @@ func TestAuthCodeResponseSuccess(t *testing.T) {
 	if err != nil {
 		t.Fatal(err)
 	}
-	req.SetBasicAuth("user02", "secret")
-	handler.ServeHTTP(w, req)
+	req.SetBasicAuth("user02", "secret") // password gets not checked as we mock the ldap server which accepts every user
+	handler.ServeHTTP(w, req)            // request an auth code. The user auth is done against the mocked ldap server
 	resp := w.Result()
 	t.Log(resp.StatusCode)
+	// auth code reponse should redirect to provided redirect_uri
 	if resp.StatusCode != http.StatusTemporaryRedirect {
 		t.Fatalf("Expected status code %v, got %v", http.StatusTemporaryRedirect, resp.StatusCode)
 	}
@@ -182,6 +184,7 @@ func TestAuthCodeResponseSuccess(t *testing.T) {
 		t.Fatalf("Redirect URI not matching. Expected %s, got %s", excpectedRedirectURI, locationURL.Scheme+"://"+locationURL.Host+locationURL.EscapedPath())
 	}
 	queryValues := locationURL.Query()
+	// the new location uri must include a code and state parameter
 	mandatoryParameters := []string{"code", "state"}
 	for _, mandatoryParameter := range mandatoryParameters {
 		if p, ok := queryValues[mandatoryParameter]; ok {
@@ -196,6 +199,7 @@ func TestAuthCodeResponseSuccess(t *testing.T) {
 	if len(queryValues) != 2 {
 		t.Fatalf("Not exact 2 query parameters given. Expected %v, got %v", mandatoryParameters, queryValues)
 	}
+	// body must be empty as the new redirect location contains all needed parameters
 	respBody, err := io.ReadAll(resp.Body)
 	if err != nil {
 		t.Fatal("Error while reading body: ", err)
@@ -207,9 +211,12 @@ func TestAuthCodeResponseSuccess(t *testing.T) {
 	wg.Wait()
 }
 
+// exchange an auth code with an access token
+// as we want to use the phantom token approach, the access token must not include structured data
+// see also https://curity.io/resources/learn/phantom-token-pattern/
 func TestOpaqueTokenResponseSuccess(t *testing.T) {
 	expectedHeaders := map[string]string{
-		"Content-Type": "application/json",
+		"Content-Type": "application/json;charset=UTF-8",
 		// authorization server MUST include the HTTP "Cache-Control"
 		// response header field [RFC2616] with a value of "no-store" in any
 		// response containing tokens, credentials, or other sensitive
@@ -258,6 +265,7 @@ func TestOpaqueTokenResponseSuccess(t *testing.T) {
 		t.Fatal(err)
 	}
 	authCode := locationURL.Query().Get("code")
+	t.Logf("AuthCode: %s", authCode)
 	locationURL.RawFragment = ""
 	locationURL.RawQuery = "" // no query in token request url, but in the body
 	reqBodyValues := url.Values{}
@@ -268,10 +276,12 @@ func TestOpaqueTokenResponseSuccess(t *testing.T) {
 	reqRedirect := httptest.NewRequest(
 		"POST",
 		locationURL.String(),
-		strings.NewReader(reqBodyValues.Encode()),
+		strings.NewReader(reqBodyValues.Encode()), // only set because of interface needs it
 	)
+	reqRedirect.PostForm = reqBodyValues // sets the content-type correct
 	b, _ := io.ReadAll(reqRedirect.Body)
-	t.Log(string(b))
+	t.Logf("Opaque token request body: %s", string(b))
+	w = httptest.NewRecorder() // new recorder required to prevent checking previous results
 	handler.ServeHTTP(w, reqRedirect)
 	respToken := w.Result()
 	if respToken.Status != "200 OK" {
@@ -286,6 +296,33 @@ func TestOpaqueTokenResponseSuccess(t *testing.T) {
 			}
 		}
 	}
+	resObj := make(map[string]interface{})
+	body, err := io.ReadAll(respToken.Body)
+	if err != nil {
+		t.Error(err)
+	}
+	errJson := json.Unmarshal(body, &resObj)
+	if errJson != nil {
+		t.Error(errJson)
+	}
+	t.Log(resObj)
+	for _, k := range []string{"access_token", "token_type", "expires_in", "refresh_token", "scope"} {
+		if _, ok := resObj[k]; !ok {
+			t.Errorf("Missing key in response access token json object: %s, got response %v", k, resObj)
+		}
+	}
+	// check for not long enough access tokens, UUIDs have 32 characters + 4 dashes
+	if len(resObj["access_token"].(string)) < 36 {
+		t.Errorf("Access token should have sufficient lenght of at least an UUID. Expected 36 characters, got %v (%s)", len(resObj["access_token"].(string)), resObj["access_token"].(string))
+	}
+	if resObj["token_type"].(string) != "bearer" {
+		t.Errorf("Expected token_type bearer, got %s", resObj["token_type"].(string))
+	}
+	if resObj["expires_in"].(float64) < 600 {
+		t.Errorf("Expected expiration of at least 600s, got %v", resObj["expires_in"].(float64))
+	}
+	// TODO: test whether the token is not a JWT as we expect only an opaque token
+
 	mockLdapServer.Close()
 	wg.Wait()
 }
