c856115433a957b61de540e84f5cbcd983914dc3
Paul Peschel <paul.peschel@posteo.de>
2024-09-02 22:03:01 +0200

    Add auth code test
    Taken from the previous ldapAuth_tests and modified to fit
    the new design. Also moved test helper functions like mock
    servers to an own file for a better overview in the tests
    file.

--- cmd/archonauth/main_test.go
+++ cmd/archonauth/main_test.go
@@ -2,14 +2,26 @@ package main
 
 import (
 	"context"
+	"io"
+	"net/http"
 	"net/http/httptest"
 	"os"
+	"sync"
 	"testing"
+
+	"github.com/pp23/ldapAuth/cmd/archonauth/test"
 )
 
-func TestRunServer(t *testing.T) {
+// global cache
+var mockMemcache test.MockMemCache = test.NewMockMemCache()
+
+func TestAuthCodeResponseSuccess(t *testing.T) {
 	ctx := context.Background()
-	ldapAuth, err := New(ctx, CreateConfig())
+	cfg := CreateConfig()
+	// next := http.HandlerFunc(func(rw http.ResponseWriter, req *http.Request) {})
+	cfg.Ldap.LogLevel = "DEBUG"
+	cfg.Ldap.URL = "ldap://localhost"
+	ldapAuth, err := New(ctx, cfg)
 	if err != nil {
 		LoggerERROR.Printf("%v", err)
 		os.Exit(1)
@@ -18,7 +30,7 @@ func TestRunServer(t *testing.T) {
 		Auth: ldapAuth,
 	}
 	handler := NewChiRouter(&authApi)
-	w := httptest.NewRecorder()
+
 	excpectedRedirectURI := "https://localhost:1234/token"
 	expectedCodeChallenge := "challenge123"
 	expectedState := "123"
@@ -27,5 +39,72 @@ func TestRunServer(t *testing.T) {
 		"http://localhost/auth?state="+expectedState+"&redirect_uri="+excpectedRedirectURI+"&client_id=abc&response_type=code&code_challenge="+expectedCodeChallenge,
 		nil,
 	)
-	handler.ServeHTTP(w, req)
+	w := httptest.NewRecorder()
+	t.Log("MockLdapServer URL: " + cfg.Ldap.URL)
+	mockLdapServer := test.MockTCPServer{}
+	mockMemcachedServer := test.MockTCPServer{}
+	wg := sync.WaitGroup{}
+	wg.Add(2)
+	// memcachedServer
+	t.Log("Start MockMemcachedServer")
+	go func() {
+		defer wg.Done()
+		mockMemcachedServer.Run(
+			11211,
+			mockMemcache.MockMemCachedMsgHandler,
+			func(err error) { t.Error("Error: ", err) },
+		)
+	}()
+	// LDAPServer
+	go func() {
+		defer wg.Done()
+		mockLdapServer.Run(
+			1389,
+			test.MockBindResponse,
+			func(err error) { t.Error("Error: ", err) /* t.Error() causes the test to fail */ },
+		)
+	}()
+	cfg.Ldap.Port = 1389
+	req.SetBasicAuth("user02", "secret") // password gets not checked as we mock the ldap server which accepts every user
+	handler.ServeHTTP(w, req)            // request an auth code. The user auth is done against the mocked ldap server
+	resp := w.Result()
+	t.Log(resp.StatusCode)
+	// auth code reponse should redirect to provided redirect_uri
+	if resp.StatusCode != http.StatusTemporaryRedirect {
+		t.Fatalf("Expected status code %v, got %v", http.StatusTemporaryRedirect, resp.StatusCode)
+	}
+	locationURL, err := resp.Location()
+	if err != nil {
+		t.Fatal("No location redirect: ", err)
+	}
+	if excpectedRedirectURI != locationURL.Scheme+"://"+locationURL.Host+locationURL.EscapedPath() {
+		t.Fatalf("Redirect URI not matching. Expected %s, got %s", excpectedRedirectURI, locationURL.Scheme+"://"+locationURL.Host+locationURL.EscapedPath())
+	}
+	queryValues := locationURL.Query()
+	// the new location uri must include a code and state parameter
+	mandatoryParameters := []string{"code", "state"}
+	for _, mandatoryParameter := range mandatoryParameters {
+		if p, ok := queryValues[mandatoryParameter]; ok {
+			if len(p) != 1 {
+				t.Fatalf("More than one \"%s\" parameter set", mandatoryParameter)
+			}
+			if p[0] == "" {
+				t.Fatalf("\"%s\" parameter not set in response", mandatoryParameter)
+			}
+		}
+	}
+	if len(queryValues) != 2 {
+		t.Fatalf("Not exact 2 query parameters given. Expected %v, got %v", mandatoryParameters, queryValues)
+	}
+	// body must be empty as the new redirect location contains all needed parameters
+	respBody, err := io.ReadAll(resp.Body)
+	if err != nil {
+		t.Fatal("Error while reading body: ", err)
+	}
+	if len(respBody) != 0 {
+		t.Fatalf("Expected no body, got %s [%v]", string(respBody), len(respBody))
+	}
+	mockMemcachedServer.Close()
+	mockLdapServer.Close()
+	wg.Wait()
 }
---
+++ cmd/archonauth/test/mocks.go
@@ -0,0 +1,193 @@
+package test
+
+import (
+	"bufio"
+	"errors"
+	"fmt"
+	"io"
+	"net"
+	"regexp"
+	"strconv"
+	"strings"
+	"time"
+
+	ber "github.com/go-asn1-ber/asn1-ber"
+)
+
+type (
+	LDAPString []byte
+	LDAPDN     LDAPString
+)
+
+type LDAPResult struct {
+	ResultCode        int
+	MatchedDN         LDAPDN
+	DiagnosticMessage LDAPString
+}
+
+func bytesToHexString(bytes []byte) string {
+	if len(bytes) <= 0 {
+		return ""
+	}
+	buf := new(strings.Builder)
+	ber.PrintBytes(buf, bytes, " ")
+	return buf.String()
+}
+
+// partly copied from https://github.com/bradfitz/gomemcache
+type serverItem struct {
+	flags   uint32
+	data    []byte
+	exp     time.Time // or zero value for no expiry
+	casUniq uint64
+}
+
+type MockMemCache map[string]serverItem
+
+func NewMockMemCache() MockMemCache {
+	return make(MockMemCache)
+}
+
+func (mockMemcache MockMemCache) MockMemCachedMsgHandler(br *bufio.Reader, bw *bufio.Writer) error {
+	writeRx := regexp.MustCompile(`^(set|add|replace|append|prepend|cas) (\S+) (\d+) (\d+) (\d+)(?: (\S+))?( noreply)?\r\n`)
+	for {
+		b, err := br.ReadSlice('\n')
+		if err != nil {
+			fmt.Printf("Read from connection: %v\r\n", err)
+			return nil
+		}
+		line := string(b)
+		fmt.Printf("string: %s", line)
+		fmt.Printf("bytes2hex: %s", bytesToHexString(b))
+
+		if strings.HasPrefix(line, "gets") {
+			key := strings.Fields(strings.TrimPrefix(line, "gets "))[0]
+			fmt.Printf("%s: [%s]", "gets", key)
+			if val, ok := mockMemcache[key]; ok {
+				fmt.Printf("%s: [%s]: %s", "gets", key, bytesToHexString(val.data))
+				fmt.Fprintf(bw, "VALUE %s %d %d %d\r\n", key, val.flags, len(val.data), val.casUniq)
+				bw.Write(val.data)
+				bw.Write([]byte("\r\n"))
+				bw.Write([]byte("END\r\n"))
+				bw.Flush()
+			} else {
+				fmt.Printf("Key not found: %s. Current cache: %v", key, mockMemcache)
+				for k := range mockMemcache {
+					fmt.Printf("%s == %s : %v", key, k, key == k)
+				}
+			}
+			continue
+		}
+		if m := writeRx.FindStringSubmatch(line); m != nil {
+			verb, key, flagsStr, exptimeStr, lenStr, casUniq, noReply := m[1], m[2], m[3], m[4], m[5], m[6], strings.TrimSpace(m[7])
+			flags, _ := strconv.ParseUint(flagsStr, 10, 32)
+			exptimeVal, _ := strconv.ParseInt(exptimeStr, 10, 64)
+			itemLen, _ := strconv.ParseInt(lenStr, 10, 32)
+			fmt.Printf("got %q flags=%q exp=%d %d len=%d cas=%q noreply=%q", verb, key, flags, exptimeVal, itemLen, casUniq, noReply)
+			body := make([]byte, itemLen+2)
+			_, err := io.ReadFull(br, body)
+			if err != nil {
+				fmt.Printf("Could not read message body: %v", err)
+				return err
+			}
+			fmt.Printf("body: %s", bytesToHexString(body[:itemLen]))
+			mockMemcache[key] = serverItem{
+				flags:   uint32(flags),
+				data:    body[:itemLen],
+				casUniq: 1,
+				exp:     time.Unix(exptimeVal, 0),
+			}
+			fmt.Printf("%s: [%s]: %v (%s)", verb, key, body, string(body))
+			bw.Write([]byte("STORED\r\n"))
+			bw.Flush()
+			continue
+		}
+		fmt.Printf("Unknown memcached command: %s", line)
+	}
+}
+
+type MockTCPServer struct {
+	hostport string
+	l        net.Listener
+	conns    []net.Conn
+	stop     bool
+}
+
+type TesTCPPServer interface {
+	Run(
+		port uint16,
+		msgHandler func(br *bufio.Reader, bw *bufio.Writer),
+		errHandler func(error),
+	) error
+	Close()
+}
+
+func (mockTcpServer *MockTCPServer) Run(
+	port uint16,
+	msgHandler func(br *bufio.Reader, bw *bufio.Writer) error,
+	errHandler func(error),
+) error {
+	mockTcpServer.stop = false
+	mockTcpServer.hostport = ":" + strconv.Itoa(int(port))
+	l, err := net.Listen("tcp", mockTcpServer.hostport)
+	if err != nil {
+		errHandler(err)
+		return err
+	}
+	mockTcpServer.l = l
+	for !mockTcpServer.stop {
+		conn, err := l.Accept()
+		if err != nil {
+			// check if server was stopped anyway (the error resulted likely from a use of closed network connection)
+			if mockTcpServer.stop {
+				// errHandler(err) // usually not an error
+				break
+			}
+			errHandler(err)
+			continue
+		}
+		fmt.Printf("New connection: %s", conn.RemoteAddr())
+		mockTcpServer.conns = append(mockTcpServer.conns, conn)
+		defer conn.Close()
+		br := bufio.NewReader(conn)
+		bw := bufio.NewWriter(conn)
+		msgErr := msgHandler(br, bw)
+		if msgErr != nil {
+			if !errors.Is(msgErr, io.EOF) {
+				fmt.Printf("msgHandler error %v", msgErr)
+				errHandler(msgErr)
+			} else {
+				fmt.Printf("Ignoring error %v", msgErr)
+			}
+		}
+		conn.Close()
+	}
+	return nil
+}
+
+func (mockTcpServer *MockTCPServer) Close() {
+	mockTcpServer.stop = true
+	if mockTcpServer.l == nil {
+		fmt.Printf("Listener was already nil of %s", mockTcpServer.hostport)
+	} else {
+		mockTcpServer.l.Close()
+	}
+	for _, c := range mockTcpServer.conns {
+		c.Close()
+	}
+}
+
+func MockBindResponse(br *bufio.Reader, bw *bufio.Writer) error {
+	// build the LDAP Bind Response packet
+	pkt := ber.Encode(ber.ClassApplication, ber.TypeConstructed, 1, nil, "Bind Response")
+	pkt.AppendChild(ber.NewInteger(ber.ClassUniversal, ber.TypePrimitive, ber.TagInteger, 0, "resultCode"))
+	pkt.AppendChild(ber.NewString(ber.ClassUniversal, ber.TypePrimitive, ber.TagUTF8String, "cn=user02", "matchedDN"))
+	pkt.AppendChild(ber.NewString(ber.ClassUniversal, ber.TypePrimitive, ber.TagUTF8String, "test", "diagnosticMessage"))
+	envelope := ber.Encode(ber.ClassUniversal, ber.TypeConstructed, ber.TagSequence, nil, "LDAP Response")
+	envelope.AppendChild(ber.NewInteger(ber.ClassUniversal, ber.TypePrimitive, ber.TagInteger, 1, "MessageID"))
+	envelope.AppendChild(pkt)
+
+	bw.Write(envelope.Bytes())
+	bw.Flush()
+	return nil
+}
